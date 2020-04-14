9610c6f01e23592b267a5d3d6918e5c8478c4b78
Gustavo Araujo Borges <gustavoaraujob1+1@gmail.com>
2020-04-14 18:00:48 -0300

    Added body-parser

--- api/app.js
+++ api/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+
+// Load middleware
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
 	res.send("Hello World");
 })
