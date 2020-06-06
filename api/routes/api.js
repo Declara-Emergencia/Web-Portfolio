06d5d5cc0370b9d1ecd94bad8400023eba1d2bf2
Lucas Pagel <lucas.pagel0@gmail.com>
2020-06-06 13:06:17 -0300

    added existing email verification on registration

--- api/routes/api.js
+++ api/routes/api.js
@@ -76,14 +76,27 @@ router.post('/login', (req, res) => {
 
 router.post('/register', (req, res) => {
     let userData = req.body
-    let user = new User(userData)
-    user.save((err, registeredUser) => {
-        if (err) {
-            console.log(err)
-        } else {
-            let payload = { subject: registeredUser._id}
-            let token = jwt.sign(payload, 'secretKey')
-            res.status(200).send({token})
+    let uuser = new User(userData)
+
+    User.findOne({ email: userData.email }, (error, user) => {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            if (user) {
+                res.status(401).send('invalid email')
+            }
+            else {
+                uuser.save((err, registeredUser) => {
+                    if (err) {
+                        console.log(err)
+                    } else {
+                        let payload = { subject: registeredUser._id }
+                        let token = jwt.sign(payload, 'secretKey')
+                        res.status(200).send({ token })
+                    }
+                })
+            }
         }
     })
 })  
