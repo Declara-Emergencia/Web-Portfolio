49d68d42fa3aaff38529a69bfe85319ecfb3fd78
Gustavo Araujo Borges <gustavoaraujob1+1@gmail.com>
2020-06-06 21:45:30 -0300

    Get user profile data with api request

--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-profile',
@@ -8,12 +9,16 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProfileComponent implements OnInit {
   user_id: string;
+  user;
 
-  constructor(private actRoute: ActivatedRoute) {
+  constructor(private actRoute: ActivatedRoute, private http: HttpClient) {
     this.user_id = this.actRoute.snapshot.params.id;
    }
 
   ngOnInit(): void {
+    this.http.get<any>('http://localhost:3000/api/user/' + this.user_id).subscribe(user => {
+            this.user = user;
+        })
   }
 
 }
